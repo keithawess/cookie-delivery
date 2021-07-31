30cba0434c5724b30ac767eb7699c86db6ea5772
keithawess <61606760+keithwess@users.noreply.github.com>
2021-07-31 16:04:56 -0500

    Adds Welcome Sign

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function LandingPage() {
-    return (
-        <div>
-            LandingPage
-        </div>
-    )
+  return (
+    <div className="bg-bisque landing-page flex">
+      <div className="bg-sign-green welcome-sign text-center text-white">
+        <div>Welcome to Cookietown</div>
+        <div className="text-small">Population: ???</div>
+        <button>Current Residents</button><button>New Residents</button>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
