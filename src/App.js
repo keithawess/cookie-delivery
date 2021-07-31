24a4457867274b8df77ee23d67a3ee49f81c0772
keithawess <61606760+keithwess@users.noreply.github.com>
2021-07-31 14:55:44 -0500

    Starts Routing to display pages during development

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import {BrowserRouter as Router, Switch, Route, Redirect, NavLink} from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+
+          <Route path="*" >
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
