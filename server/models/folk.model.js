12b4548f619b447c6ec44182483f5cf9d0cd7424
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-02 13:49:54 -0500

    Adds addNeighbor to route

--- server/models/folk.model.js
+++ server/models/folk.model.js
@@ -1,15 +1,18 @@
 const query = require("../config/mysql.conf");
 
 async function addNeighbor(res, neighbor) {
+
   let json = { success: false, data: null, error: null };
   try {
     const neighbors = await query("SELECT * FROM folk WHERE address = ?", [
       neighbor.address,
     ]);
+
     if (neighbors.length !== 0) {
       json.error =
         "Somebody already lives at this address. Please choose another address.";
     } else {
+
       await query(
         "INSERT INTO folk (name, address, house, face, color, roundness, height) VALUES (?,?,?,?,?,?,?)",
         [
@@ -22,12 +25,14 @@ async function addNeighbor(res, neighbor) {
           neighbor.height,
         ]
       );
-      json = {...json, success: true, data: "Congratulations! The neighbor is all moved in!"}
+      json = {...json, success: true, data: "Congratulations! The neighbor has moved in!"}
     }
   } catch (err) {
+      console.log(err);
+
     json.error = "Failed to add neighbor";
   } finally {
-    res.send(json);
+    return res.send(json);
   }
 }
 
--- server/routes/folk.routes.js
+++ server/routes/folk.routes.js
@@ -1,5 +1,60 @@
 const express = require("express");
 const router = express.Router();
-const {} = require("../models/folk.model");
+const Filter = require("bad-words");
+const { addNeighbor } = require("../models/folk.model");
+let filter = new Filter();
+
+router.post("/add", (req, res) => {
+  const { neighbor } = req.body;
+
+  if (validNeighbor(neighbor)) {
+    return addNeighbor(res, neighbor);
+  } else {
+    res.send({
+      success: false,
+      data: null,
+      error: "Invalid data provided",
+    });
+  };
+});
+
+function validNeighbor(neighbor) {
+  let { address, house, face, color, roundness, height } = neighbor;
+  let output = true;
+
+  if (neighbor.name) {
+    filter.isProfane(neighbor.name) ? (neighbor.name = "Bobby Pottymouth") : "";
+  } else {
+    output = false;
+  }
+  if (
+    address &&
+    address.split(" ").length === 3 &&
+    house &&
+    house < 3 &&
+    face &&
+    face < 4 &&
+    color &&
+    roundness &&
+    roundness <= 50 &&
+    height &&
+    height <= 100
+  ) {
+    arr = address.split(" ");
+    if (
+      isNaN(arr[0]) ||
+      ((arr[1] !== "Butterscotch" &&
+        arr[1] !== "Gingerbread") ||
+        (arr[2] !== "Ave" &&
+        arr[2] !== "St"))
+    ) {
+      output = false;
+    }
+  } else {
+    output = false;
+  }
+
+  return output;
+}
 
 module.exports = router;
