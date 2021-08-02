92a8bb751f7475e7f4ae52ae8fae7f9f614fbac0
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-01 21:34:33 -0500

    adds signup model

--- server/models/users.model.js
+++ server/models/users.model.js
@@ -2,4 +2,26 @@ const query = require("../config/mysql.conf");
 const bcrypt = require("bcrypt");
 const { v4: uuidv4 } = require("uuid");
 
-module.exports = {};
+async function signup(res, username, password){
+    let json = { success: false, data: null, error: null};
+    try {
+        const users = await query("SELECT * FROM users WHERE username = ?", [
+            username,
+        ]);
+        if (users.length !== 0) {
+            json.error = "Please choose another username."
+        } else {
+            const hashed = await bcrypt.hash(password, 10);
+            const uuid = uuidv4();
+            await query("INSERT INTO users (uuid, username, password) VALUES (?,?,?)", [uuid, username, password]);
+            json = { ...json, success: true, data: "Signup was successful!"};
+        }
+
+    } catch (err) {
+        json.error = "Signup failed";
+    } finally {
+        return res.send(json);
+    }
+}
+
+module.exports = {signup};
