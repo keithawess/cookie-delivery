93a43e36d7cd3456e8b1b0166eeec3c44e2e76d3
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-02 16:05:37 -0500

    Adds inputs and button

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Signup() {
-    return (
+  const [usernameInput, setUsernameInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+
+  return (
+    <div>
+      Signup
+      <form>
         <div>
-            Signup
+          <label htmlFor="username">Username: </label>
+          <input
+            value={usernameInput}
+            onChange={(e) => {
+              setUsernameInput(e.target.value);
+            }}
+            id="username"
+          />
         </div>
-    )
+
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            value={passwordInput}
+            onChange={(e) => {
+              setPasswordInput(e.target.value);
+            }}
+          />
+        </div>
+        
+        <button>
+            Signup
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
