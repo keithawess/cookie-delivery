1c1a2ff0360a6c8639e9577b965f63bfb2eebf13
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-03 11:47:06 -0500

    Adds config file

---
+++ server/config/passport.conf.js
@@ -0,0 +1,60 @@
+const LocalStrategy = require("passport-local");
+const { Strategy } = require("passport-jwt");
+const jwt = require("jsonwebtoken");
+const { login, getByUuid } = require("../models/users.model");
+
+function configPassport(passport) {
+  passport.use(
+    "local-login",
+    new LocalStrategy(async (username, password, done) => {
+      const { data, error } = await login(username, password);
+      if (error) {
+        return done(error);
+      }
+
+      const token = jwt.sign({ uuid: data.uuid }, process.env.SECRET_KEY, {
+        expiresIn: "12h",
+      });
+
+      return done(
+        null,
+        {
+          username: data.username,
+        },
+        { token }
+      );
+    })
+  );
+
+  const cookieJWTExtractor = (req) => {
+    let token = null;
+    if (req && req.cookies) {
+      token = req.cookies["jwt"];
+    }
+    return token;
+  };
+
+  const jwtOptions = {
+    secretOrKey: process.env.SECRET_KEY,
+    jwtFromRequest: cookieJWTExtractor,
+  };
+
+  passport.use("jwt", new Strategy(jwtOptions, async (payload, done) => {
+      if(!payload || !payload.uuid) {
+          return done(true, false, "Invalid Credentials");
+      }
+      const {data, error} = await getByUuid(payload.uuid);
+      if (error)
+      {
+          return done(true, false, "Invalid credentials");
+      }
+      return done(false, data, null);
+  }));
+
+  passport.serializeUser((user, done) => {
+      done(null, user.id);
+  })
+
+}
+
+module.exports = configPassport;
