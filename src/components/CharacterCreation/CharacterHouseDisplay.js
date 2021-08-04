65df8400fb7e07d4d61cdad4c6c24d891e358dd2
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-04 14:10:37 -0500

    Adds house display for neighbors

--- src/components/CharacterCreation/CharacterHouseDisplay.js
+++ src/components/CharacterCreation/CharacterHouseDisplay.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import house1 from './images/house1.png'
+import house2 from './images/house2.png'
+import house3 from './images/house3.png'
+const houseArr = [house1, house2, house3]
 
-function CharacterHouseDisplay() {
+function CharacterHouseDisplay({house}) {
     return (
         <div>
-            CharacterHouseDisplay
+            <img className="house" src={houseArr[house]}/>
         </div>
     )
 }
