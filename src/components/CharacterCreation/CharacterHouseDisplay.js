bfa8ea7ddd83d5c0772ca6d405df306e7ff923c1
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-04 14:17:00 -0500

    Makes house sizing more flexible

--- src/components/CharacterCreation/CharacterHouseDisplay.js
+++ src/components/CharacterCreation/CharacterHouseDisplay.js
@@ -4,10 +4,10 @@ import house2 from './images/house2.png'
 import house3 from './images/house3.png'
 const houseArr = [house1, house2, house3]
 
-function CharacterHouseDisplay({house}) {
+function CharacterHouseDisplay({house, height}) {
     return (
         <div>
-            <img className="house" src={houseArr[house]}/>
+            <img style={{maxHeight: height + 'px'}} className={`house`} src={houseArr[house]}/>
         </div>
     )
 }
