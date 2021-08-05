56f3543f59c940290c5e78d089c27b43a1fdd167
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-05 14:06:31 -0500

    Adds functionality to name/height/roundess/color

--- src/components/CharacterCreation/CharacterCreationPage.js
+++ src/components/CharacterCreation/CharacterCreationPage.js
@@ -9,7 +9,7 @@ const faces = [sad, happy, skeptic, smile];
 
 function CharacterCreationPage() {
   const [step, setStep] = useState("0");
-  const [height, setHeight] = useState(50);
+  const [height, setHeight] = useState(75);
   const [roundness, setRoundness] = useState(0);
   const [color, setColor] = useState("#FFFFFF");
   const [name, setName] = useState("");
@@ -31,35 +31,60 @@ function CharacterCreationPage() {
         />
       </div>
       <div className="margin-5">
-        <label htmlFor="nameInput">Name:</label>
-        <input id="nameInput" />
+        <label htmlFor="nameInput">Name: </label>
+        <input
+          id="nameInput"
+          value={name}
+          onChange={(e) => {
+            if (e.target.value.length <= 20) setName(e.target.value);
+          }}
+        />
       </div>
       <div className="flex input-container">
-          <div className="margin-5 flex-third">
-            <div>
-              <label htmlFor="heightBar">Height:</label>
-              <input id="heightBar" type="range" className="slider" />
-            </div>
-
-            <div>
-              <label htmlFor="roundnessBar">Roundness:</label>
-              <input id="roundnessBar" type="range" className="slider" />
-            </div>
+        <div className="margin-5 flex-third">
+          <div>
+            <label htmlFor="heightBar">Height:</label>
+            <input
+              id="heightBar"
+              type="range"
+              min="50"
+              max="100"
+              value={height}
+              onChange={(e) => {
+                setHeight(e.target.value);
+              }}
+              className="slider"
+            />
           </div>
 
-          <div className="margin-5 flex-third">
-            <label htmlFor="colorSelector">Color:</label>
-            <input id="colorSelector" type="color" />
+          <div>
+            <label htmlFor="roundnessBar">Roundness:</label>
+            <input id="roundnessBar" type="range"   min="0"
+              max="50"
+              value={roundness}
+              onChange={(e) => {
+                setRoundness(e.target.value);
+              }} className="slider" />
           </div>
+        </div>
 
-          <div className="margin-5 flex-third">
-            <label htmlFor="faceSelector">Face:</label>
-            <div className="flex wrap">
-              {faces.map((val) => {
-                return <div className="margin-5 flex flex-half justify-space-around align-items-center"><img className="neighbor-face" src={val} /></div>;
-              })}
-            </div>
+        <div className="margin-5 flex-third">
+          <label htmlFor="colorSelector">Color:</label>
+          <input id="colorSelector" type="color" value={color} onChange={(e)=>{setColor(e.target.value)}}/>
+        </div>
+
+        <div className="margin-5 flex-third">
+          <label htmlFor="faceSelector">Face:</label>
+          <div className="flex wrap">
+            {faces.map((val) => {
+              return (
+                <div className="margin-5 flex flex-half justify-space-around align-items-center">
+                  <img className="neighbor-face" src={val} />
+                </div>
+              );
+            })}
           </div>
+        </div>
       </div>
     </div>
   );
