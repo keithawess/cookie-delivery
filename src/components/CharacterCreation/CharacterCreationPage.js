074a937404a651ff583ff205a021bede58a87951
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-05 12:32:18 -0500

    Adds planned state for future use

--- src/components/CharacterCreation/CharacterCreationPage.js
+++ src/components/CharacterCreation/CharacterCreationPage.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CharacterHouseDisplay from "./CharacterHouseDisplay";
 import CharacterDisplay from './CharacterDisplay';
 
 function CharacterCreationPage() {
+    const [step, setStep] = useState("0");
+    const [height, setHeight] = useState(50);
+    const [roundness, setRoundness] = useState(0);
+    const [color, setColor] = useState("#FFFFFF");
+    const [name, setName] = useState("");
+    const [face, setFace] = useState(0);
+    const [addressNum, setAddressNum] = useState(0);
+    const [addressStName, setAddressStName] = useState("Gingerbread");
+    const [addressStType, setAddressStType] = useState("Ave");
+
     return (
         <div className={"bg-white"}>
             CharacterCreactionPage
-            <CharacterDisplay name={"Bobby"} face={2} height={100} house={2} roundness={3} color={"#1a9393"} height={50}/>
-            <CharacterHouseDisplay/>
+
+        <CharacterDisplay name={name} face={face} color={color} roundness={roundness} height={height} />
+
+
         </div>
     )
 }
