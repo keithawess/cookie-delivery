cf9d85f4668ea5781381952d1b9291c06b1e309d
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-06 12:31:52 -0500

    Displays house and neighbor to page

--- src/components/CharacterCreation/CharacterInteractionPage.js
+++ src/components/CharacterCreation/CharacterInteractionPage.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CharacterHouseDisplay from "./CharacterHouseDisplay";
 import { NeighborContext } from "../../context";
+import CharacterDisplay from "./CharacterDisplay";
 
 function CharacterInteractionPage() {
   const { visitAddress, neighborMsg, setNeighborMsg, currNeighbor } =
@@ -16,12 +17,15 @@ function CharacterInteractionPage() {
 
   return (
     <div>
-      CharacterInteractionPage
-      <div className="absolute abs-center z-back">
+      {currNeighbor && <div>{currNeighbor.name}'s House</div>}
+      <div>
         {currNeighbor && (
-          <div>
-            <CharacterHouseDisplay house={currNeighbor.house} height={200} />
-          </div>
+          <>
+            <div className="absolute abs-center z-back">
+              <CharacterHouseDisplay house={currNeighbor.house} height={200} />
+            </div>
+            <div className="absolute abs-center z-middle"><CharacterDisplay color={currNeighbor.color} height={currNeighbor.height} face={currNeighbor.face} roundness={currNeighbor.roundness} /></div>
+          </>
         )}
         {!currNeighbor && neighborMsg && <div>{neighborMsg}</div>}
       </div>
