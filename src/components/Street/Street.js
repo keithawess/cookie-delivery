15285bd52bbf91868214c09210a143bb8c49e7f8
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-06 10:44:23 -0500

    Displays houses to street

--- src/components/Street/Street.js
+++ src/components/Street/Street.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Redirect, Route } from "react-router-dom";
+import { NeighborContext } from "../../context";
+import CharacterHouseDisplay from "../CharacterCreation/CharacterHouseDisplay";
 
 function Street() {
-    return (
-        <div>
-            Street
+  const { neighborhood } = useContext(NeighborContext);
+
+  return (
+    <div>
+      Street
+      <div className="flex width-95 justify-space-evenly">
+        <div className="flex-half flex col">
+          {neighborhood
+            .filter((house, i) => i < 5)
+            .map((house, i) => {
+              return (
+                <div key={i + 100} onClick={()=>{<Redirect to="/"/>}}>
+                  <CharacterHouseDisplay
+                    key={i}
+                    height={100}
+                    house={house.house}
+                  />
+                </div>
+              );
+            })}
         </div>
-    )
+        <div className="flex-half flex col">
+          {neighborhood
+            .filter((house, i) => i >= 5)
+            .map((house, i) => {
+              return (
+                <div key={i + 100} onClick={()=>{<Redirect to="/"/>}}>
+                  <CharacterHouseDisplay
+                    key={i}
+                    height={100}
+                    house={house.house}
+                  />
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Street
+export default Street;
