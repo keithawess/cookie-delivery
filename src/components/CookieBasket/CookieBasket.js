aefc4b409cd19051e500941c3fffaea3fd6a2de4
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-06 15:26:22 -0500

    Creates cookie display

--- src/components/CookieBasket/CookieBasket.js
+++ src/components/CookieBasket/CookieBasket.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from "react";
+import CookieDisplay from "./CookieDisplay";
 
-function CookieBasket() {
-    let cookies = [1,2,3,4,5,6,7,8,9,0];
-    return (
-        <div>
-            <div className="absolute basket flex wrap">
-                {cookies.map((cookie)=> {
-                    return <div className="border flex-fifth flex justify-center align-items-center">{cookie}</div>
-                }
-                )}
+function CookieBasket({ cookie }) {
+  let cookies = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  return (
+    <div>
+      <div className="absolute basket flex wrap">
+        {cookies.map((cookie, i) => {
+          return (
+            <div className="border flex-fifth flex justify-center align-items-center">
+              <CookieDisplay cookie={0} />
             </div>
-        </div>
-    )
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default CookieBasket
+export default CookieBasket;
--- src/components/CookieBasket/CookieDisplay.js
+++ src/components/CookieBasket/CookieDisplay.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import cookie1 from "./images/cookie1.png";
+import cookie2 from "./images/cookie2.png";
+import cookie3 from "./images/cookie3.png";
+const cookieList = [cookie1, cookie2, cookie3];
 
-function CookieDisplay() {
+function CookieDisplay({cookie}) {
     return (
         <div>
-            CookieDisplay
+            <img className="cookie" src={cookieList[cookie]}/>
         </div>
     )
 }
