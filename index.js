291208aba79eda9a9c4f76b6846d288e22a42ba5
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-08 22:49:35 -0500

    Reverts index back to developent

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ const folkRoutes = require("./server/routes/folk.routes");
 
 app.use(express.json());
 
-app.enable('trust proxy');
-app.use((req, res, next) => {
-    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
-});
+// app.enable('trust proxy');
+// app.use((req, res, next) => {
+//     req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+// });
 
 passportConf(passport);
 app.use(cookieParser());
-app.use(express.static(__dirname + "/build"));
+// app.use(express.static(__dirname + "/build"));
 app.use(passport.initialize());
 
 app.use("/api/users", userRoutes);
 app.use("/api/folk", folkRoutes);
 
-app.get("*", (req, res) => {
-    return res.sendFile("/build/index.html", {root: __dirname + "/" });
-})
+// app.get("*", (req, res) => {
+//     return res.sendFile("/build/index.html", {root: __dirname + "/" });
+// })
 
 app.listen(PORT, () => console.log("Connected"));
